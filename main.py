c209b3a0ed04ed6fc0792c89704f80c91ee63ad3
Christopher <tuj80083@temple.edu>
2019-11-07 12:10:24 -0500

    created new folder to break modules between discord commands and bot commands. Also added blank test files within both to verify both folders are properly accessed and imported from

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import discord
 import os #gives access to listdir()
 import importlib
 
-commandDir = 'commands' #where modules are stored
+discordCommandDir = 'discordCommands' #storage for discord affecting modules
+botCommandDir = 'botCommands' #storage for bot related modules
 commandList = []
 buffer = []
 TOKEN = open("faadbotToken.txt", "r").read() #Gets OAuth2 Token for bot
@@ -11,18 +12,36 @@ client = discord.Client()
 
 @client.event #bootup event
 async def on_ready():
-    print('Logged in as ' + client.user.name + ' ' + client.user.id)
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
     print('-----')
 
-    try:
-        for commandFile in os.listdir(commandDir):
+    try: #parses and imports botCommand modules
+        for commandFile in os.listdir(botCommandDir):
             if commandFile != '__init__.py' and commandFile.endswith(".py"): #make sure not to import init file
                 buffer = commandFile.split('.')
                 #print(buffer[0])
-                foundCommand = importlib.import_module(commandDir + "." + buffer[0])
+                foundCommand = importlib.import_module(botCommandDir + "." + buffer[0])
                 print(foundCommand)
                 commandList.append(buffer[0]) #adds module command to list of commands
     except FileNotFoundError:
         print('No command files found')
+    try: #parses and imports discordCommand modules
+        for commandFile in os.listdir(discordCommandDir):
+            if commandFile != '__init__.py' and commandFile.endswith(".py"): #make sure not to import init file
+                buffer = commandFile.split('.')
+                #print(buffer[0])
+                foundCommand = importlib.import_module(discordCommandDir + "." + buffer[0])
+                print(foundCommand)
+                commandList.append(buffer[0]) #adds module command to list of commands
+    except FileNotFoundError:
+        print('No command files found')
+
+    #verify inputs in terminal
+    print('Commands that have been imported:\n')
+    for com in commandList:
+        print(com + ' ')
+    print('\n')
 
 client.run(TOKEN) #runs bot, should be last line
