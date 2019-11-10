94959c73e59abfdc3d47d6ab60aaec42865499e8
Christopher Park <tuj80083@temple.edu>
2019-11-10 16:32:48 -0500

    refactored main to use cogs. Created first instance of cog for reference.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os #gives access to listdir()
 import importlib
+import sys
+from discord.ext import commands
 
 discordCommandDir = 'discordCommands' #storage for discord affecting modules
 botCommandDir = 'botCommands' #storage for bot related modules
@@ -8,40 +10,59 @@ commandList = []
 buffer = []
 TOKEN = open("faadbotToken.txt", "r").read() #Gets OAuth2 Token for bot
 
-client = discord.Client()
 
-@client.event #bootup event
+def get_prefix(bot, message):
+
+    # Notice how you can use spaces in prefixes. Try to keep them simple though.
+    prefixes = ['!']
+
+    # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+bot = commands.Bot(command_prefix= get_prefix, description = 'bot')
+
+cogs = ['cogs.startup']
+#populate list of cogs
+try: #parses and adds botCommand modules
+    for commandFile in os.listdir(botCommandDir):
+        if commandFile != '__init__.py' and commandFile.endswith(".py"): #dont want to add init file
+            buffer = commandFile.split('.')
+            foundCommand = botCommandDir + "." + buffer[0]
+            #print(foundCommand + "\n") #testing
+            commandList.append(foundCommand)
+except FileNotFoundError:
+    print('No command files found')
+
+try: #parses and adds discordCommand modules
+    for commandFile in os.listdir(discordCommandDir):
+        if commandFile != '__init__.py' and commandFile.endswith(".py"): #dont want to add init file
+            buffer = commandFile.split('.')
+            foundCommand = discordCommandDir + "." + buffer[0]
+            #print(foundCommand + "\n") #testing
+            commandList.append(foundCommand)
+except FileNotFoundError:
+    print('No command files found')
+
+if __name__ == '__main__':
+    for extension in commandList:
+        #print(extension + "\n")
+        bot.load_extension(extension)
+
+@bot.event #bootup event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('-----')
-
-    try: #parses and imports botCommand modules
-        for commandFile in os.listdir(botCommandDir):
-            if commandFile != '__init__.py' and commandFile.endswith(".py"): #make sure not to import init file
-                buffer = commandFile.split('.')
-                #print(buffer[0])
-                foundCommand = importlib.import_module(botCommandDir + "." + buffer[0])
-                print(foundCommand)
-                commandList.append(buffer[0]) #adds module command to list of commands
-    except FileNotFoundError:
-        print('No command files found')
-    try: #parses and imports discordCommand modules
-        for commandFile in os.listdir(discordCommandDir):
-            if commandFile != '__init__.py' and commandFile.endswith(".py"): #make sure not to import init file
-                buffer = commandFile.split('.')
-                #print(buffer[0])
-                foundCommand = importlib.import_module(discordCommandDir + "." + buffer[0])
-                print(foundCommand)
-                commandList.append(buffer[0]) #adds module command to list of commands
-    except FileNotFoundError:
-        print('No command files found')
 
     #verify inputs in terminal
     print('Commands that have been imported:\n')
     for com in commandList:
-        print(com + ' ')
+        if com not in sys.modules:
+            print('You have not imported the {} module'.format(com))
+        else:
+            print(com + ' ')
     print('\n')
 
-client.run(TOKEN) #runs bot, should be last line
+#client.run(TOKEN) #runs bot, should be last line
+bot.run(TOKEN, bot=True, reconnect=True)
