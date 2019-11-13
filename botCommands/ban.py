79547c9d91d5babbfd71c0858970bda9afb878eb
Christopher Park <tuj80083@temple.edu>
2019-11-13 10:21:01 -0500

    added @has_permissions to ban/kick commands so users require permissions to access

--- botCommands/ban.py
+++ botCommands/ban.py
@@ -1,11 +1,13 @@
 import discord 
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 class ban(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
 
     @commands.command(name = 'ban')
+    @has_permissions(manage_roles=True, ban_members=True)
     async def ban(self,ctx, member : discord.Member, *, reason=None):
         await member.ban(reason=reason)
 
