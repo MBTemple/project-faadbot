f7655b7c4bd22827d4a73fc892ccc9780964372f
Christopher Park <tuj80083@temple.edu>
2019-11-19 20:33:40 -0500

    Add role assignment for one-night roles, werewolf

--- cogs/werewolfMan.py
+++ cogs/werewolfMan.py
@@ -10,21 +10,22 @@ class werewolfMan(commands.Cog):
 
     @commands.command(name = 'Wstartup')
     async def Wstartup(self, ctx):
-        await ctx.send('Initializing werewolf mod.')
-        await ctx.send('Please input your name to join the game')
-        await ctx.send('Join Syntax: !?join PlayerName')
-        await ctx.send('Change roles Syntax: !?roleSetting')
-        await ctx.send('Game Start Syntax: !?beginGame')
-        await ctx.send('Ideally you should join in the order you are sitting')
-        f = open("cogs/log/werewolfData.txt", "w")
-        g = open("cogs/log/werewolfRoles.txt", "w")
+        await ctx.send("Initializing werewolf mod. \n \
+Please input your name to join the game \n \
+Join Syntax: ``!join Playername`` \n \
+Change roles Syntax: ``!roleSetting`` \n \
+Game Start Syntax: ``!beginGame`` \n \
+Ideally you should join in the order you are sitting")
+        
+        f = open("cogs/werewolfData.txt", "w")
+        g = open("cogs/werewolfRoles.txt", "w")
         #f.write("Players:\n")
         f.close()
         g.close()
 
     @commands.command(name = 'roleSetting')
     async def roleSetting(self, ctx, arg1 : str, arg2 : int):
-        g = open("cogs/log/werewolfRoles.txt", "a")
+        g = open("cogs/werewolfRoles.txt", "a")
         #only including default setting for now
         if arg1 == "default" and arg2 < 16:
             g.write("Werewolf:2")
@@ -35,7 +36,7 @@ class werewolfMan(commands.Cog):
 
     @commands.command(name = 'join')
     async def join(self, ctx, arg1):
-        f = open("cogs/log/werewolfData.txt", "a")
+        f = open("cogs/werewolfData.txt", "a")
         f.write(str(ctx.message.author.id) + ":" + arg1 + "\n")
         await ctx.send(arg1 + ' has joined the game')
         f.close()
@@ -46,47 +47,61 @@ class werewolfMan(commands.Cog):
         getID = []
         playerCount = 0
         await ctx.send("beginning game with players:")
-        f = open("cogs/log/werewolfData.txt", "r")
-        for line in f:
-            if playerCount >= 0 and not line.split():
-                getID = line.split(':')
-                await ctx.send(getID[1])
-                user = self.bot.get_user(int(getID[0]))
-                await user.send("You are registered for the round") #works as reply
-                await user.send("Use this chat to make actions each round")
-            playerCount+=1
-        #f.close() #commenting this out since werewolfData is used for assigning roles
+        with open("cogs/werewolfData.txt", "r") as f:
+            for line in f:
+                if playerCount >= 0:
+                    getID = line.split(':')
+                    await ctx.send(getID[1])
+                    user = self.bot.get_user(int(getID[0]))
+                    await user.send("You are registered for the round") #works as reply
+                    await user.send("Use this chat to make actions each round")
+                playerCount+=1
+
+        if playerCount == 0:
+            await ctx.send("no players in game")
 
         if(playerCount < 7): #forces one night rules if less than 7 players
-            roles = open("cogs/werewolfRoles.txt", "w")
-            roles.write("one night:default\n") #rule description
-            roles.write("Werewolf:0\nWerewolf:0\nSeer:0\nRobber:0\nTroublemaker:0\nVillager:0\n") #total of 6 roles
+            await ctx.send("defaulting to one night rules due to party size")
+            with open("cogs/werewolfRoles.txt", 'w') as roles:
+                roles.write("one night:default\nWerewolf:0\nWerewolf:0\nSeer:0\nRobber:0\nTroublemaker:0\nVillager:0\n") #rule description
+
+            #roles.write("Werewolf:0\nWerewolf:0\nSeer:0\nRobber:0\nTroublemaker:0\nVillager:0\n") #total of 6 roles
             #number after colon in werewolfRoles determines if role is taken or not. 1 means claimed
         
-        noWerewolf = True
+        #await ctx.send("assigning roles, standby for messages")
+        noWerewolf = True #false to skip over this temporarily for testing
         while(noWerewolf):
-            g = open("cogs/log/werewolfRound.txt", "w") #used to store round by round info
+            g = open("cogs/werewolfRound.txt", "w") #used to store round by round info
+            f = open("cogs/werewolfData.txt", "r")
             #will player information as "userID:PlayerName:Role:GameStatus" with GameStatus being alive or dead
-            for line in f:
+            #await ctx.send("Assigning player roles")
+            for line in f:   
+                roles = open("cogs/werewolfRoles.txt", "r")            
                 openRoles = []
                 for role in roles:
                     tempRoles = []
                     tempRoles = role.split(":")
-                    print(tempRoles[0])
-                    if tempRoles[1] == "0":
+                    if tempRoles[1] == "0\n":
                         openRoles.append(tempRoles[0])
-                    print("Werewolf Management: Role " + tempRoles[0] + " added to openRoles")
+                        print("Werewolf Management: Role " + tempRoles[0] + " added to openRoles")
+                roles.close()
                 chosenRole = random.choice(openRoles)
+                openRoles.remove(chosenRole)
+                #await ctx.send(chosenRole) #here to test
                 if chosenRole == "Werewolf":
                     noWerewolf = False
-                player = line[:-2]
+                player = line[:-1]
                 print("Werewolf Management: Chosen role for " + player + " is: " + chosenRole)
                 g.write(player + ":" + chosenRole + ":Alive\n")
+                roles = open("cogs/werewolfRoles.txt", "w")
+                for role in openRoles:
+                    roles.write(role + ":0\n")
 
                 
         roles.close()
         g.close()
         f.close()
+        await ctx.send("role assignment complete")
 
 
 def setup(bot):
